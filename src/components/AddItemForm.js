a3ef967b0325113f4b7d070b31092d613ce72b19
codingSlate <ananta2021k@gmail.com>
2021-11-16 11:10:05 +0530

    2 files added

---
+++ src/components/AddItemForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddItemForm.css';
+import Form from './Form';
+const AddItemForm = () => {
+  return (
+    <div>
+      <Form />
+      <h2>Item List</h2>
+    </div>
+  );
+};
+export default AddItemForm;
