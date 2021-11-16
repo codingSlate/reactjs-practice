8414642306bfa838d59404368177539f51cb27bf
codingSlate <ananta2021k@gmail.com>
2021-11-16 14:01:51 +0530

    Child to Parent data sent from Form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div>
       <h1>Task List</h1>
-      {/* <p>Task list management</p> */}
+      <p>Task list management</p>
       <AddItemForm />
     </div>
   );
