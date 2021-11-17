f21f700c2f973e83af26ad5bf26bd13de6c73fda
codingSlate <ananta2021k@gmail.com>
2021-11-17 11:08:35 +0530

    2 files modified

--- src/components/ShowItemList.js
+++ src/components/ShowItemList.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './ShowItemList.css';
+
 const ShowItemList = (props) => {
-  console.log(props.task);
+  // console.log(props.task);
   if (props.task.length === 0) {
     return <div clssName="center">No data found</div>;
   }
   return (
     <ul clssName="nostyle">
-      {props.task.map((t) => (
-        <li className="" key={t.id}>
+      {props.task.map((t, index) => (
+        <li className="li" key={index}>
+          <span>#{index} </span>
           {t.itemName}
+          <span>Edit</span>
+          <span>Delete</span>
         </li>
       ))}
-      {/* {props.task[0].name} */}
-      {/* {props.task.map((t, index) => {
-        <li key={index}>{t.itemName}</li>;
-      })} */}
     </ul>
   );
 };
